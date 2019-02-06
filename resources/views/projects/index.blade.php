884411f2c5ae2f426167398b96687f2a7bf3b6cb
Kevin Prunty <Kevinp@clickandgo.local>
2019-02-06 09:37:27 +0000

    adds a new model, view, controller for Project

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Projects</title>
+   </head>
+   <body>
+            
+      <nav>
+         <ul>
+            <li><a href="/">home</a></li>
+            <li><a href="/contact">contact</a></li>
+            <li><a href="/about">about</a></li>
+         </ul>
+      </nav>
+
+      <h1>Projects</h1>
+
+      <ul>
+         @foreach($projects as $project)
+            <li><?= $project->title; ?></li>
+         @endforeach
+      </ul>
+      
+
+   </body>
+</html>
